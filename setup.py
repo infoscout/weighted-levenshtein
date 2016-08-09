44d23dec317fd1ff6dd712f3a16300c58bba651c
InfoScout Corp <corp@infoscoutinc.com>
2016-08-09 02:11:21 +0100

    oops

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import run_setup
-
+import os
 
 run_setup('weighted_levenshtein/setup.py', ['build_ext', '--inplace'])
 
--- weighted_levenshtein/setup.py
+++ weighted_levenshtein/setup.py
@@ -1,7 +1,8 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import os
 
 setup(
     name='clev',
-    ext_modules=cythonize("clev.pyx"),
+    ext_modules=cythonize(os.path.join(os.path.dirname(__file__), "clev.pyx")),
 )
